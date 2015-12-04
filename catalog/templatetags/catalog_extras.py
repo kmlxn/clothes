b7a39bbc6ffd0873e2cd290a41b18aefe26f9ae8
John Doe <JohnDoe@email.com>
2015-12-04 19:10:03 +0500

    Clothes grid moved to separate partial template.

--- catalog/templatetags/catalog_extras.py
+++ catalog/templatetags/catalog_extras.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.inclusion_tag('catalog/filters_partial.html')
 def render_filters(filters_list):
     return {'filters_list': filters_list}
+
+@register.inclusion_tag('catalog/clothes_partial.html')
+def render_clothes_grid(clothes):
+    return {'clothes': clothes}
